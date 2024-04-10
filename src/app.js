6ed6f667846ce15a958dc3de912d892eb2599d94
PabloElectricista <pablo_spector@hotmail.com>
2024-04-09 23:18:17 -0300

    cambio del contenido servido

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import routes from './routes/index.js';
 import './config/db.js'
 
 const app = express();
-app.set('view engine', 'ejs')
+app.use(express.static('dist'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
