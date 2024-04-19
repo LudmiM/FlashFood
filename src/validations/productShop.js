d589e768705d043729b7f844c5f974659ad14301
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-04-18 23:29:14 -0300

    Edito validaciones

--- src/validations/productShop.js
+++ src/validations/productShop.js
@@ -5,10 +5,6 @@ module.exports = [
         .notEmpty().withMessage('El nombre no puede estar vacio'),
     body('price')
         .notEmpty().withMessage('El precio no puede estar vacio'),
-    body('currency_type')
-        .notEmpty().withMessage('El tipo de cambio no puede estar vacio'),
-    body('time')
-        .notEmpty().withMessage('El tiempo de preparacion no puede estar vacio'),
     body('description')
         .notEmpty().withMessage('El debe agregar una descripcion'),
     body('idCategory')
