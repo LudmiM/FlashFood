ead53bd62f028fba1e1a336788c802c0a12a3a3b
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-04-20 18:11:55 -0300

    Add config for controllers deleteCommerce

--- src/controllers/Commerce/deleteCommerce.js
+++ src/controllers/Commerce/deleteCommerce.js
@@ -2,3 +2,43 @@
 //Buscar todos los productos e ir eliminando
 //Eliminr la ubicaion/locacion del comercio
 //Eliminar el comercio
+const db = require('../../database/models');
+
+module.exports = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await db.Product.destroy(
+            {
+                where: {
+                    idCommerce: id
+                }
+            }
+        );
+
+        const commerce = await db.Commerce.destroy(
+            {
+                where: {
+                    id: id
+                },
+                include: [{ model: db.Location, as: 'location' }]
+            }
+        );
+
+        const respuesta = {
+            meta: {
+                status: 200,
+                data: commerce,
+                url: `api/commerce/delete/${id}`
+            }
+        };
+
+        res.status(201).json(respuesta);
+    } catch (error) {
+        console.error('Error al editar el comercio:', error);
+        res.status(error.status || 500).json({
+            ok: false,
+            msg: error.message || "Upss, hubo un error. Sorry!",
+        });
+    }
+};
